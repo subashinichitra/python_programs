f51b43d9f035420100b9313da3e755ab69896ded
subashinichitra <subashinir2025@gmail.com>
2026-06-16 08:24:34 +0530

    Prime Programs

--- basic_programs/PrimeNumbers/prime_number.py
+++ basic_programs/PrimeNumbers/prime_number.py
@@ -1,11 +1,11 @@
-num=int(input("ENTER THE NUMBER:"))
+n=int(input("ENTER THE NUMBER:"))
 
-if num <= 1:
-    print(f"{num} is not a prime number")
+if n <= 1:
+    print(f"{n} is not a prime number")
 else:
-    for i in range(2,num):
-        if num % i == 0:
+    for i in range(2,n):
+        if n % i == 0:
             print(f"{n} is not a prime number")
             break
     else:
-            print(f"{num} is a prime number")
+            print(f"{n} is a prime number")
