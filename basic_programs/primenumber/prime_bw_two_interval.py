8897b7c24e86ba42efd72ac37b1f002eefff3ca9
subashinichitra <subashinir2025@gmail.com>
2026-06-16 08:19:01 +0530

    PRIME NUMBERS

--- basic_programs/primenumber/prime_bw_two_interval.py
+++ basic_programs/primenumber/prime_bw_two_interval.py
@@ -1,6 +1,6 @@
-start=int(input("ENTER THE STARTING NUMBER:"))
-end=int(input("ENTER THE ENDING NUMBER:"))
-for num in range(start,end+1):
+starting=int(input("ENTER THE STARTING NUMBER:"))
+ending=int(input("ENTER THE ENDING NUMBER:"))
+for num in range(starting,ending+1):
     for i in range(2,num):
         if num % i == 0:
             break
