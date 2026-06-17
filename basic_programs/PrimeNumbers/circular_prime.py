a4889dcd51cd01a13f9c1e99d69c19caf6127d23
subashinichitra <subashinir2025@gmail.com>
2026-06-17 07:34:23 +0530

    prime numbers

---
+++ basic_programs/PrimeNumbers/circular_prime.py
@@ -0,0 +1,19 @@
+
+def prime(tmp):
+    for i in range(len(temp)):
+        rotate=int(temp[i:]+temp[:i])
+        print(rotate)
+
+        for j in range(2,rotate):
+            if rotate % j ==0:
+                flag=1
+                break
+
+number=int(input("ENTER THE NUMBER: "))
+temp=str(number)
+flag=0
+prime(temp)
+if flag==0:
+     print(f"THE GIVEN NUMBER {number} IS A CIRCULAR PRIME")
+else:
+    print(f"THE GIVEN NUMBER {number} IS NOT A CIRCULAR PRIME")
