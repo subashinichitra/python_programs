54f52e182ce2d2673a476a54d884cd4468c6433d
subashinichitra <subashinir2025@gmail.com>
2026-06-19 21:14:32 +0530

    Array Basics

---
+++ basic_programs/ARRAYS/ARRAY_BASICS/linear_search.py
@@ -0,0 +1,11 @@
+arr = [10,20,30,40]
+search = int(input("Enter search value: "))
+flag = False
+for i in arr:
+    if i == search:
+        flag = True
+        break
+if flag:
+    print("Element Found")
+else:
+    print("Element Not Found")
