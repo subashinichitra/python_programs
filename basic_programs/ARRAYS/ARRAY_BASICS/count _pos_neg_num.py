8f0824e36144a7b9824c58e877baa7bc3ab50cfc
subashinichitra <subashinir2025@gmail.com>
2026-06-19 21:10:24 +0530

    Array Basics

---
+++ basic_programs/ARRAYS/ARRAY_BASICS/count _pos_neg_num.py
@@ -0,0 +1,10 @@
+arr = [10,-5,20,-3,8]
+positive = 0
+negative = 0
+for i in arr:
+    if i > 0:
+        positive = positive + 1
+    elif i < 0:
+        negative = negative + 1
+print("Positive =", positive)
+print("Negative =", negative)
