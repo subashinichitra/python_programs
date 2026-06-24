dd51900950ecbcfeace9919e74f3c8239034a096
subashinichitra <subashinir2025@gmail.com>
2026-06-24 20:33:13 +0530

    Array Basics

--- basic_programs/ARRAYS/ARRAY_BASICS/find_index_pos.py
+++ basic_programs/ARRAYS/ARRAY_BASICS/find_index_pos.py
@@ -2,5 +2,5 @@ arr = [10,20,30,40]
 search = int(input("Enter value: "))
 for i in range(len(arr)):
     if arr[i] == search:
-        print("Index value =", i)
+        print("Index value is  ", i)
         break
